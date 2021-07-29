d2d9cef6a2b534d4e611962c45409f82cee9b9ae
David Park <david@watermelonfueled.com>
2021-07-29 10:27:03 -0400

    fix minor warnings/errors

--- src/pages/Episode.tsx
+++ src/pages/Episode.tsx
@@ -229,7 +229,7 @@ const ResponseGrid = (
 const ChangeEpisode = ({ currentId }: { currentId: null | string }) => {
   const episodes = useEpisodesList()
 
-  const currentIndex = episodes.findIndex(({ id }) => id === currentId)
+  const currentIndex = episodes?.findIndex(({ id }) => id === currentId) ?? -1
 
   return currentIndex === -1 ? null : (
     <div className="grid grid-cols-2 mt-auto py-16 px-4 gap-4 w-128 max-w-full self-center">
